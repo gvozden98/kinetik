f78c262bf3cc0082b740d36e83ca3cfeeee37389
gvozden98 <ognjen.gvozden98@gmail.com>
2023-04-24 02:04:21 +0200

    test

---
+++ src/library.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style/style.css";
+import { Container, Row, Col } from "react-bootstrap";
+const Library = () => {
+  return (
+    <Container className="mt-2">
+      <Row>
+        <Col sm></Col>
+        <Col sm></Col>
+      </Row>
+    </Container>
+  );
+};
+export default Library;
